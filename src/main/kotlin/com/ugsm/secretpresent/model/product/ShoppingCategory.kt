4d0505c7848e35be505e71f03354a33472775102
khkim <kkh147.17.3@gmail.com>
2024-01-15 18:31:11 +0900

    feat: update

--- src/main/kotlin/com/ugsm/secretpresent/model/product/ShoppingCategory.kt
+++ src/main/kotlin/com/ugsm/secretpresent/model/product/ShoppingCategory.kt
@@ -4,15 +4,13 @@ import com.fasterxml.jackson.annotation.JsonBackReference
 import jakarta.persistence.*
 
 @Entity
-class ProductCategory(
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) var id: Long,
+class ShoppingCategory(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) var id: Long,
 
     @ManyToOne
     @JoinColumn(name="product_id")
     @JsonBackReference
     var product: Product,
 
-    @ManyToOne
-    @JoinColumn(name="shopping_category_id")
-    var category: ShoppingCategory
 )
