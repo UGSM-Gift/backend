faa7f4914de84381c5ca10f0e61e5074eca34039
khkim <kkh147.17.3@gmail.com>
2024-03-08 16:28:31 +0900

    fix: remove @EnableWebMvc

--- src/main/kotlin/com/ugsm/secretpresent/config/WebConfig.kt
+++ src/main/kotlin/com/ugsm/secretpresent/config/WebConfig.kt
@@ -12,11 +12,10 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.format.FormatterRegistry
-import org.springframework.web.servlet.config.annotation.EnableWebMvc
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import java.text.SimpleDateFormat
 
-@EnableWebMvc
 @Configuration
 class WebConfig(
     @Autowired val userActivityInterceptor: UserActivityInterceptor
