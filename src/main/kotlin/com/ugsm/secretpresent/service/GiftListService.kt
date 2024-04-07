6cf75b1b5b50d7d151fffe7959fd34b669689cda
kkh <kkh147.17.3@gmail.com>
2024-04-07 22:53:40 +0900

    feat: add transaction

--- src/main/kotlin/com/ugsm/secretpresent/service/GiftListService.kt
+++ src/main/kotlin/com/ugsm/secretpresent/service/GiftListService.kt
@@ -189,6 +189,7 @@ class GiftListService(
         )
     }
 
+    @Transactional
     fun getInfo(giftListId: Int): GiftListInfoDto {
         val giftListProductCategories = giftListProductCategoryRepository.findByGiftListId(giftListId)
         val giftListProductCategoriesDto = giftListProductCategories.map {
